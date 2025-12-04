c6cd3e4f43312b47d89583ba8a866b3d309f9f63
triducta-uit <ductt@uit.edu.vn>
2025-12-04 13:43:54 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -17,7 +17,7 @@ module tt_um_asiclab_example (
 );
 
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = 0
+  assign uo_out  = 0;
   assign uio_out = 0;
   assign uio_oe  = 0;
 
